451c5d2be2e83efb003428d8e6115931de490239
Mijail U <mijailwwe@hotmail.com>
2024-12-18 12:24:06 -0500

    Clases - Constructor con parametros

---
+++ bitboss/04-clases.py
@@ -0,0 +1,24 @@
+# Metodo constructor que reciba sus valores
+
+class Personaje:
+    nombre = "Default"
+    fuerza = 0
+    inteligenia = 0
+    defensa = 0
+    vida = 0
+    
+    # Esta es la forma de declarar un constructor con argumentos
+    """ self es un argumento que hace referencia asi mismo al objeto,
+        y permite a traves del punto tener acceso a los atributos y 
+        metodos de la clase"""
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida):   # los atributos que usemos para inicializar el objeto
+        self.nombre = nombre # nuestro objetivo es cambiar el nombre de la clase por el nombre que recibe del constructor
+        self.fuerza = fuerza # lo mismo con la fuerza
+        self.inteligenia = inteligencia
+        self.defensa = defensa
+        self.vida = vida 
+
+# ahora le tenemos que pasar los atributos al constructor que hemos especificado antes
+mi_personaje = Personaje("BitBoss", 10, 1, 5, 100)
+print("El nombre del jugador es", mi_personaje.nombre)    # a traves del punto podemos consultar cada uno de los atributos
+print("La fuerza del jugador es", mi_personaje.fuerza)
