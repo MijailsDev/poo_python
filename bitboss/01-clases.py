cd918efe494b517c4a0e3d9822ea170394beaead
Mijail U <mijailwwe@hotmail.com>
2024-12-18 11:23:24 -0500

    Clases - Personaje basico

---
+++ bitboss/01-clases.py
@@ -0,0 +1,11 @@
+# Vamos a crear la clase Personaje
+
+class Personaje:
+    pass
+
+
+# para crear un objeto con esta plantilla solo tienes
+# que pasar el metodo constructor de la clase 
+
+mi_personaje = Personaje()      # el constructor lo guardamos en una variable
+print(mi_personaje)
---
+++ bitboss/02-clases.py
@@ -0,0 +1,9 @@
+class Personaje:
+    nombre = "Default"
+    fuerte = 0
+    inteligenia = 0
+    defensa = 0
+    vida = 0
+
+mi_personaje = Personaje()
+print(mi_personaje) # nos devuelve lo mismo
