e01005e43047faff6d8f630d314ccf9264b78fb9
Mijail <mijailwwe@hotmail.com>
2024-12-19 00:09:51 -0500

    Abstraccion - Metodo para poder subir de nivel a nuestro personaje

---
+++ bitboss/08-clases.py
@@ -0,0 +1,32 @@
+# Abstraccion
+# Metodo constructor que reciba sus valores
+
+class Personaje:
+
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida):   # los atributos que usemos para inicializar el objeto
+        self.nombre = nombre # nuestro objetivo es cambiar el nombre de la clase por el nombre que recibe del constructor
+        self.fuerza = fuerza # lo mismo con la fuerza
+        self.inteligencia = inteligencia
+        self.defensa = defensa
+        self.vida = vida 
+
+# Metodo que nos muestre el listado del objeto(el valor de sus atributos)
+    def atributos(self): # como se va a encargar de mostrar los atributos, solo necesita el self para poder tener acceso a ellos
+        print(self.nombre, ":", sep="")
+        print(".Fuerza:", self.fuerza)
+        print(".Inteligenicia", self.inteligencia)
+        print(".Defensa", self.defensa)
+        print(".Vida", self.vida)
+
+# Metodo para poder subir de nivel a nuestro personaje, sumandole estadisticas nuevas a las actuales         
+    def subir_nivel(self, fuerza, inteligencia, defensa): # self para poder modificar los atributos
+        self.fuerza = self.fuerza + fuerza
+        self.inteligencia = self.inteligencia + inteligencia
+        self.defensa = self.defensa + defensa
+
+
+# ahora le tenemos que pasar los atributos al constructor que hemos especificado antes
+mi_personaje = Personaje("BitBoss", 10, 1, 5, 100) # creamos un objeto a partir de la clase Personaje
+mi_personaje.atributos() # llamamos a la funcino a traves de la variable mi_personaje
+mi_personaje.subir_nivel(1, 2, 0)   # subimos de nivel al personaje
+mi_personaje.atributos()    # volvemos a mostrar los atributos
