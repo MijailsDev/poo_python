2929c8366670adf6d1f5572adb9fe0392d2e0bfb
mijail-ubuntu <mijailwwe@hotmail.com>
2024-12-19 09:10:36 -0500

    Abstraccion - Metodo que calcule el daño de nuestro Personaje a otro Personaje

---
+++ bitboss/11-clases.py
@@ -0,0 +1,44 @@
+# Abstraccion
+# Metodo constructor que reciba sus valores
+
+class Personaje:
+
+    def __init__(self, nombre, fuerza, inteligencia, defensa, vida):   # los atributos que usemos para inicializar el objeto
+        self.nombre = nombre # nuestro objetivo es cambiar el nombre de la clase por el nombre que recibe del constructor
+        self.fuerza = fuerza # lo mismo con la fuerza
+        self.inteligencia = inteligencia
+        self.defensa = defensa
+        self.vida = vida 
+
+# Metodo que nos muestre el listado del objeto(el valor de sus atributos)
+    def atributos(self): # como se va a encargar de mostrar los atributos, solo necesita el self para poder tener acceso a ellos
+        print(self.nombre, ":", sep="")
+        print(".Fuerza:", self.fuerza)
+        print(".Inteligenicia", self.inteligencia)
+        print(".Defensa", self.defensa)
+        print(".Vida", self.vida)
+
+# Metodo para poder subir de nivel a nuestro personaje, sumandole estadisticas nuevas a las actuales         
+    def subir_nivel(self, fuerza, inteligencia, defensa): # self para poder modificar los atributos
+        self.fuerza = self.fuerza + fuerza
+        self.inteligencia = self.inteligencia + inteligencia
+        self.defensa = self.defensa + defensa
+
+# Metodo para saber si el personaje esta vivo o no
+    def esta_vivo(self):    # Este metodo nos devuelve un valor booleano
+        return self.vida > 0 # comprueba si la vida es mayor que cero, True o False
+    
+# Metodo que representa la accion de morir del Personaje    - si su vida == 0 --> esta muerto
+    def morir(self):
+        self.vida = 0
+        print(self.nombre, "ha muerto")
+
+# Metodo que calcule el daño de nuestro Personaje a otro Personaje
+    def daño(self, enemigo):  # el daño que recibe nuestro Personaje, y al otro personaje que llamaremos enemigo
+        return self.fuerza - enemigo.defensa  # vamos a devolver el daño realizado de una forma muy simple
+    
+# ahora le tenemos que pasar los atributos al constructor que hemos especificado antes
+mi_personaje = Personaje("BitBoss", 10, 1, 5, 100) 
+mi_enemigo = Personaje("Enemy Stando", 8, 5, 3, 100)    # creamos un personaje que representa al enemigo 
+print(mi_personaje.daño(mi_enemigo))    #  probamos el metodo
+
